2b746d2bded494c8925ff3414df9a7e15bf39777
millatip <pratiwimillati@gmail.com>
2019-01-10 22:57:48 +0700

    Update NetworkService.kt

--- app/src/main/java/com/dicoding/millatip/footballapps/data/network/NetworkService.kt
+++ app/src/main/java/com/dicoding/millatip/footballapps/data/network/NetworkService.kt
@@ -1,11 +1,15 @@
 package com.dicoding.millatip.footballapps.data.network
 
+import com.dicoding.millatip.footballapps.data.model.LeagueResponse
 import com.dicoding.millatip.footballapps.data.model.MatchResponse
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NetworkService {
+
+    @GET("v1/json/1/all_leagues.php")
+    fun getLeagueList(): Deferred<LeagueResponse>
 
     @GET("v1/json/1/eventspastleague.php")
     fun getPreviousMatch(@Query("id") leagueId: String) : Deferred<MatchResponse>
