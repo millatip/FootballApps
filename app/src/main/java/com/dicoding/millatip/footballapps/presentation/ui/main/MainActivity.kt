d6c253d6e45a26968871e93489385e56267377e4
millatip <pratiwimillati@gmail.com>
2019-01-11 09:08:12 +0700

    update main activity

--- app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/main/MainActivity.kt
@@ -2,12 +2,40 @@ package com.dicoding.millatip.footballapps.presentation.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import com.dicoding.millatip.footballapps.R
+import com.dicoding.millatip.footballapps.presentation.ui.favorite.FavoriteFragment
+import com.dicoding.millatip.footballapps.presentation.ui.match.MatchFragment
+import com.dicoding.millatip.footballapps.presentation.ui.team.TeamFragment
+import com.dicoding.millatip.footballapps.utils.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViewPager(viewPager)
+        bottomNavListener()
+    }
+
+    private fun bottomNavListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.action_match -> viewPager.currentItem = 0
+                R.id.action_team -> viewPager.currentItem = 1
+                R.id.action_favorite -> viewPager.currentItem = 2
+
+            }
+            true
+        }
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(MatchFragment(), "Match")
+        adapter.addFragment(TeamFragment(), "Team")
+        adapter.addFragment(FavoriteFragment(), "Favorite")
+        viewPager.adapter = adapter
     }
 }
