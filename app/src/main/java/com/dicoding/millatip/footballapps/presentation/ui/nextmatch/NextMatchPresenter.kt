fb6307a80cbf1f49fcedb47589e1b68f433eca4c
millatip <pratiwimillati@gmail.com>
2019-01-11 15:05:01 +0700

    surround with try and catch

--- app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/nextmatch/NextMatchPresenter.kt
+++ app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/nextmatch/NextMatchPresenter.kt
@@ -6,23 +6,33 @@ import com.dicoding.millatip.footballapps.presentation.base.BasePresenter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class NextMatchPresenter <V : NextMatchContract.View>
 constructor(private val matchRepository: MatchRepository, private val leagueRepository: LeagueRepository) : BasePresenter<V>(), NextMatchContract.UserActionListener<V>{
     override fun getLeagueList() {
-        GlobalScope.launch(Dispatchers.Main){
-            val data = leagueRepository.getSoccerLeagueList()
-            view?.displayLeagueList(data)
+        try {
+            GlobalScope.launch(Dispatchers.Main){
+                val data = leagueRepository.getSoccerLeagueList()
+                view?.displayLeagueList(data)
+            }
+        }catch (e: Exception){
+            e.printStackTrace()
         }
     }
 
     override fun getMatchList() {
         view?.showLoading()
         val leagueId = view?.selectedLeague?.leagueId
-        GlobalScope.launch(Dispatchers.Main){
-            val data = matchRepository.getNextMatch(leagueId.toString())
-            view?.displayMatchList(data)
-            view?.hideLoading()
+        try {
+            GlobalScope.launch(Dispatchers.Main){
+                val data = matchRepository.getNextMatch(leagueId.toString())
+                view?.displayMatchList(data)
+                view?.hideLoading()
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
         }
+
     }
 }
