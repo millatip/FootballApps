3ce5c070078d78588a870ce2162b6b21a078ff04
millatip <pratiwimillati@gmail.com>
2019-01-11 15:22:14 +0700

    move recyclerview adapter

--- app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/nextmatch/NextMatchFragment.kt
+++ app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/nextmatch/NextMatchFragment.kt
@@ -126,27 +126,5 @@ class NextMatchFragment : Fragment(), NextMatchContract.View {
         super.onDestroyView()
     }
 
-    internal class NextMatchAdapter(private val matches: List<Match>, val notificationListener: (Match) -> Unit) : RecyclerView.Adapter<NextMatchAdapter.ViewHolder>(){
-        override fun onCreateViewHolder(parent: ViewGroup, p1: Int): NextMatchAdapter.ViewHolder = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.next_match_list, parent, false))
-
-        override fun getItemCount(): Int = matches.size
-
-        override fun onBindViewHolder(holder: NextMatchAdapter.ViewHolder, position: Int) {
-            holder.bindItem(matches[position])
-        }
-
-        inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-            fun bindItem(match: Match){
-                val date = dateFormatter(match.matchDate)
-                val time = timeFormatter(match.matchTime)
-
-                itemView.tvHomeTeam.text = match.homeTeamName
-                itemView.tvAwayTeam.text = match.awayTeamName
-                itemView.tvDateTime.text = toGmtFormat("$date $time")
-                itemView.ivNotification.setOnClickListener { notificationListener(match) }
-            }
-        }
-
-    }
 
 }
