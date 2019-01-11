bea96546614262026054df8178364c706ebfa95d
millatip <pratiwimillati@gmail.com>
2019-01-11 17:11:51 +0700

    optimize imports

--- app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/main/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.dicoding.millatip.footballapps.presentation.ui.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
 import com.dicoding.millatip.footballapps.R
-import com.dicoding.millatip.footballapps.presentation.ui.favorite.FavoriteFragment
 import com.dicoding.millatip.footballapps.presentation.ui.match.MatchFragment
-import com.dicoding.millatip.footballapps.presentation.ui.team.TeamFragment
 import com.dicoding.millatip.footballapps.utils.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
