856c0f507aca3854da6f5d668d3d3e03a6b8caa2
millatip <pratiwimillati@gmail.com>
2019-01-23 16:18:42 +0700

    add ui test

---
+++ app/src/androidTest/java/com/dicoding/millatip/footballapps/presentation/ui/main/InstrumentationTest.kt
@@ -0,0 +1,48 @@
+package com.dicoding.millatip.footballapps.presentation.ui.main
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.pressBack
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView
+import com.dicoding.millatip.footballapps.R.id.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class InstrumentationTest {
+    @Rule
+    @JvmField var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testAppBehaviour(){
+        Thread.sleep(2500)
+
+        onView(withId(rvPrevMatch)).check(matches(isDisplayed()))
+        onView(withId(rvPrevMatch)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(6))
+        onView(withId(rvPrevMatch)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(6, click()))
+
+        onView(withId(add_to_fav)).check(matches(isDisplayed()))
+        onView(withId(add_to_fav)).perform(click())
+        Thread.sleep(3000)
+        pressBack()
+
+        onView(withId(bottomNavigationView)).check(matches(isDisplayed()))
+        onView(withId(action_fav)).check(matches(isDisplayed()))
+        onView(withId(action_fav)).perform(click())
+
+        onView(withId(rvFavoriteMatch)).check(matches(isDisplayed()))
+        onView(withId(rvFavoriteMatch)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
+        onView(withId(rvFavoriteMatch)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(add_to_fav)).check(matches(isDisplayed()))
+        onView(withId(add_to_fav)).perform(click())
+        onView(withText("Removed from favorite")).check(matches(isDisplayed()))
+    }
+}
