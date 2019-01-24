b86d342441e71df4170d00a8dc002a6f693b8cce
millatip <pratiwimillati@gmail.com>
2019-01-24 15:19:49 +0700

    add favoritematchpresenter unit test

--- app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/favoriteMatch/FavoriteMatchPresenter.kt
+++ app/src/main/java/com/dicoding/millatip/footballapps/presentation/ui/favoriteMatch/FavoriteMatchPresenter.kt
@@ -2,25 +2,24 @@ package com.dicoding.millatip.footballapps.presentation.ui.favoritematch
 
 import com.dicoding.millatip.footballapps.data.repository.match.MatchRepository
 import com.dicoding.millatip.footballapps.presentation.base.BasePresenter
+import com.dicoding.millatip.footballapps.utils.CoroutineContextProvider
 import com.dicoding.millatip.footballapps.utils.EspressoIdlingResource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class FavoriteMatchPresenter<V : FavoriteMatchContract.View>
-constructor(private val matchRepository: MatchRepository) : BasePresenter<V>(),
+constructor(private val matchRepository: MatchRepository,
+            private val context: CoroutineContextProvider = CoroutineContextProvider()
+) : BasePresenter<V>(),
     FavoriteMatchContract.UserActionListener<V> {
     override fun getFavoriteMatchList() {
-        EspressoIdlingResource.increment()
         view?.showLoading()
-        GlobalScope.launch(Dispatchers.Main) {
+        GlobalScope.launch(context.main) {
             try {
                 val data = matchRepository.getFavoriteMatches()
                 view?.displayFavoriteMatchList(data)
                 view?.hideLoading()
-                if (!EspressoIdlingResource.idlingResource.isIdleNow){
-                    EspressoIdlingResource.decrement()
-                }
             } catch (e: Exception) {
                 e.printStackTrace()
                 view?.hideLoading()
---
+++ app/src/test/java/com/dicoding/millatip/footballapps/presentation/ui/favoritematch/FavoriteMatchPresenterTest.kt
@@ -0,0 +1,56 @@
+package com.dicoding.millatip.footballapps.presentation.ui.favoritematch
+
+import com.dicoding.millatip.footballapps.data.model.FavoriteMatch
+import com.dicoding.millatip.footballapps.data.model.Match
+import com.dicoding.millatip.footballapps.data.repository.match.MatchRepository
+import com.dicoding.millatip.footballapps.presentation.ui.prevmatch.PrevMatchContract
+import com.dicoding.millatip.footballapps.utils.TestContextProvider
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+class FavoriteMatchPresenterTest {
+
+    @Mock
+    private lateinit var matchRepository: MatchRepository
+
+    @Mock
+    private lateinit var view: FavoriteMatchContract.View
+
+    @Mock
+    private lateinit var match: List<FavoriteMatch>
+
+    private lateinit var presenter: FavoriteMatchPresenter<FavoriteMatchContract.View>
+
+    @Before
+    fun setUp(){
+        MockitoAnnotations.initMocks(this)
+
+        presenter = FavoriteMatchPresenter(matchRepository, TestContextProvider())
+        presenter.onAttach(view)
+    }
+
+    @Test
+    fun shouldDisplayFavoriteMatchListWhenGetDataSuccess(){
+        runBlocking {
+            `when`(matchRepository.getFavoriteMatches()).thenReturn(match)
+
+            presenter.getFavoriteMatchList()
+
+            verify(view).showLoading()
+            verify(view).displayFavoriteMatchList(match)
+            verify(view).hideLoading()
+        }
+    }
+
+    @After
+    fun tearDown() {
+        presenter.onDetach()
+    }
+
+}
